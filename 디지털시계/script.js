e8a99cbd7ab8a51a926b03fbdc79dfcfc513d72c
devellybutton <127278410+devellybutton@users.noreply.github.com>
2024-02-03 22:39:00 +0900

    24/02/03

---
+++ 디지털시계/script.js
@@ -0,0 +1,12 @@
+// 업그레이드할 예정
+// 요소 선택
+const todayDiv = document.querySelector("#today");
+const timeDiv = document.querySelector("#time");
+
+// getTime 함수
+function getTime() {
+    const date = new Date();
+
+}
+
+// setInterval 함수
